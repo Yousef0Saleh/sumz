2bb26f222282a432d19a20d148fa5f3007d411c6
Yousef0Saleh <yousef.programmer100@gmail.com>
2024-09-24 22:58:40 +0300

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-white text-center font-inter py-4 w-full fixed bottom-0">
+      By Yousef Saleh
+    </footer>
+  );
+};
+
+export default Footer;
